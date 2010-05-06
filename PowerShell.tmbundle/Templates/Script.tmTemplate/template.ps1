719b2f484cb0f855e6196880c52df9ab0b1528cf
David Peckham <dave.peckham@me.com>
2010-05-05 21:57:46 -0700

    first commit

---
+++ PowerShell.tmbundle/Templates/Script.tmTemplate/template.ps1
@@ -0,0 +1,41 @@
+# 
+# ${TM_NEW_FILE_BASENAME}.ps1
+# 
+# Created by ${TM_FULLNAME} on ${TM_DATE}.
+# Copyright (c) ${TM_YEAR} ${TM_ORGANIZATION_NAME}. All rights reserved.
+# 
+# .SYNOPSIS 
+# tbd
+# 
+# .DESCRIPTION
+# tbd
+# 
+# To run this script from Control-M:
+# powershell.exe '.\${TM_NEW_FILE_BASENAME}.ps1'
+# 
+# .PARAMETER param1
+# tbd
+# 
+# .PARAMETER OutputPath
+# tbd
+# 
+# .INPUTS
+# tbd
+# 
+# .OUTPUTS
+# tbd
+# 
+# .EXAMPLE
+# C:\PS> .\${TM_NEW_FILE_BASENAME}.ps1
+# 
+
+
+param
+(
+	$param1, 
+	$param2 = "a default value"
+)
+
+# more here...
+
+exit 0
